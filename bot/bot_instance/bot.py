e0563d3c82883e9e35e502dffbe03390f9f3e7b3
vedaukr <kalispo121221@gmail.com>
2024-03-19 13:45:26 +0100

    Removed unused google vision functionality.

--- bot/bot_instance/bot.py
+++ bot/bot_instance/bot.py
@@ -12,8 +12,6 @@ bot_instance.set_my_commands([
     telebot.types.BotCommand("/shazam_song", "Shazam song"),
     telebot.types.BotCommand("/get_dementia_rating", "Dementia rating"),
     telebot.types.BotCommand("/get_rtf", "Получить пожилого ртфченка"),
-    telebot.types.BotCommand("/get_img_labels", "Описание картинки (реплай на картинку)"),
-    telebot.types.BotCommand("/get_img_text", "Распознать текст на картинке (реплай на картинку)"),
     telebot.types.BotCommand("/get_bot_triggers", "Че ботяра умеет"),
     telebot.types.BotCommand("/get_gpt_params", "GPT parameters (list, comment, range)"),
     telebot.types.BotCommand("/reset_gpt_params", "Reset GPT parameters to default (retards from chat have set some shit values)"),
