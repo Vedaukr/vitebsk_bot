43f2db8a8ff02978f0eeecba0e5e9d9e8a104745
vedaukr <kalipso121221@gmail.com>
2025-02-05 15:32:06 +0100

    Changed default llm mapping.

--- bot/handlers/configs/llm_config.py
+++ bot/handlers/configs/llm_config.py
@@ -6,8 +6,9 @@ from services.llm.anthropic_llm import AnthropicLlm
 default_model_mapping: dict[str, LlmModel] = {}
 
 # openai
-default_model_mapping["gpt"] = OpenAiLlm(model_name="gpt-4o-mini") 
-default_model_mapping["гпт"] = OpenAiLlm(model_name="gpt-4o-mini") 
+default_model_mapping["gpt4m"] = OpenAiLlm(model_name="gpt-4o-mini")
+default_model_mapping["gpt"] = OpenAiLlm(model_name="gpt-4o") 
+default_model_mapping["гпт"] = OpenAiLlm(model_name="gpt-4o") 
 default_model_mapping["gpt4"] = OpenAiLlm(model_name="gpt-4o") 
 default_model_mapping["гпт4"] = OpenAiLlm(model_name="gpt-4o") 
 default_model_mapping["o1"] = OpenAiO1Llm(model_name="o1-mini") 
